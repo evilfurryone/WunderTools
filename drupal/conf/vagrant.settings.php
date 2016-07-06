782e102b6b96fff216770e410ff1d3177e000df4
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-07-06 15:53:55 +0300

    Updated the settings.php with configuration that fits the current setup.

--- drupal/conf/vagrant.settings.php
+++ drupal/conf/vagrant.settings.php
@@ -1,11 +1,11 @@
 <?php
 
-// DATABASE
+// Database
 
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'drupal',
       'username' => 'drupal',
@@ -18,16 +18,20 @@ $databases = array (
   ),
 );
 
-// CACHING
+// Memcache
 $conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
 $conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 $conf['cache_default_class'] = 'MemCacheDrupal';
 $conf['memcache_key_prefix'] = 'wk';
+// Use memcache as the system lock to avoid deadlock (which occur easily with semaphore in database)
+$conf['lock_inc'] = 'sites/all/modules/contrib/memcache/memcache-lock.inc';
 
-$conf['cache_backends'][] = 'sites/all/modules/contrib/varnish/varnish.cache.inc';
-$conf['cache_class_cache_page']  = 'VarnishCache';
+// Varnish reverse proxy on 127.0.01
+$conf['reverse_proxy'] = TRUE;
+$conf['reverse_proxy_addresses'] = array('127.0.0.1');
+$conf['varnish_version'] = "4";
+$conf['varnish_control_key'] = 'secret-key-here';
+$conf['varnish_control_terminal'] = '127.0.0.1:6082';
 
-// Varnish
-$conf['varnish_version'] = "3";
+// Environment indicator
 $conf['simple_environment_indicator'] = '#88b700 Local';
-
